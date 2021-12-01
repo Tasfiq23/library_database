e62d2ff2f9cbc6daa8025254b5225fec42cc4550
Tasfiq Asif <ggunnerr@gmail.com>
2021-12-02 08:59:21 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from models import Base, session,Book,engine
 
 if __name__=='__main__':
     Base.metadata.create_all(engine)
-# import models file 
+
+# imports models file 
 # main menu -add,search,analysis,exit,view
 # add books  to the database
 # edit books

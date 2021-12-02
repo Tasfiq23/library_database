9c45058dedb29feec64979db8c4ed684c25825b4
Tasfiq23 <ggunnerr@gmail.com>
2021-12-02 23:37:08 +1100

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,31 @@ def clean_price(price_str):
               \rEX: 20.00
               \rPress enter to try again.
               \r********************''')
-        
+        return      
     else:
         return int(price * 100)
+    
+    
+def clean_id(id_str,options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+              \n*****ID ERROR****
+              \rThe id format should be a number without any symbol
+              \rPress enter to try again.
+              \r********************''')  
+        return 
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+              \n*****ID ERROR****
+              \rOptions:{options}
+              \rPress enter to try again.
+              \r********************''') 
+            return  
         
 
 
@@ -78,6 +100,7 @@ def app():
     while app_running:
         choice = menu()
         if choice == '1':
+            # Add books
             title=input("Title: ")
             author =  input("Author: ")
             date_error = True
@@ -92,20 +115,35 @@ def app():
                 price= clean_price(price)
                 if type(price) == int:
                     price_error = False
-            new_boook = Book(title = title,author=author,published_date=date,price=price)
-            session.add(new_boook)
+            new_book = Book(title = title,author=author,published_date=date,price=price)
+            session.add(new_book)
             session.commit()
             print('Book is Added')
             time.sleep(1.5)
                     
 
         elif choice== '2':
+            # View Books
             for book in session.query(Book):
                 print(f'{book.id} | {book.title} | {book.author}')
             input('\nPress Enter to return to the main menu')
-            pass
+            
         elif choice== '3':
-            pass
+            #Search book
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error= True
+            while id_error:    
+                id_choice=input(f'''
+                    \nId Options: {id_options}
+                    \rBook id: ''')
+                id_choice = clean_id(id_choice,id_options)
+                if type(id_choice) == int:
+                    id_error=False
+            the_book = session.query(Book).filter(Book.id==id_choice)
+            print(f'''\n{the_book.title} by {the_book.author} \rPublished:{the_book.published_date} \rPrice: ${the_book.price/100}''')
+                    
         elif choice== '4':
             pass
         else:
